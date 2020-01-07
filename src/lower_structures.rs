2f63f970c1a1f8aad3245f94af4061a173abedd3
Frank Dai <fydai@berkeley.edu>
2020-01-07 15:23:10 -0800

    Preliminary reference counting analysis

--- src/lower_structures.rs
+++ src/lower_structures.rs
@@ -1,7 +1,10 @@
+use crate::data::flat_ast as flat;
 use crate::data::low_ast as low;
 use crate::data::repr_specialized_ast_alt as special;
 use crate::util::graph::{self, Graph};
 use crate::util::id_vec::IdVec;
+use im_rc::OrdMap;
+use im_rc::OrdSet;
 use std::collections::BTreeSet;
 
 fn lower_type(type_: &special::Type) -> low::Type {
@@ -134,6 +137,185 @@ fn find_boxed_types(
         box_infos,
     )
 }
+
+#[derive(Clone, Debug)]
+struct MoveInfo {
+    move_info: OrdMap<flat::LocalId, u64>,
+}
+
+#[derive(Clone, Debug)]
+struct FutureUsageInfo {
+    future_usages: OrdSet<flat::LocalId>,
+}
+
+#[derive(Clone, Debug)]
+struct AnnotExpr {
+    kind: AnnotExprKind,
+    move_info: MoveInfo,
+}
+
+#[derive(Clone, Debug)]
+enum AnnotExprKind {
+    Branch(
+        flat::LocalId,
+        Vec<(special::Condition, AnnotExpr)>,
+        special::Type,
+    ),
+    LetMany(
+        Vec<(special::Type, AnnotExpr, FutureUsageInfo)>,
+        flat::LocalId,
+    ),
+    Leaf(special::Expr),
+}
+
+fn annotate_expr(
+    expr: special::Expr,
+    future_usages: &FutureUsageInfo,
+    ids_in_scope: usize,
+) -> AnnotExpr {
+    match expr {
+        special::Expr::LetMany(exprs, final_local) => {
+            let mut current_future_usages = future_usages.clone();
+            current_future_usages.future_usages.insert(final_local);
+
+            let mut let_move_info = MoveInfo {
+                move_info: OrdMap::new(),
+            };
+            if final_local.0 < ids_in_scope {
+                if !current_future_usages.future_usages.contains(&final_local) {
+                    let_move_info.move_info.insert(final_local, 1);
+                } else {
+                    let_move_info.move_info.insert(final_local, 0);
+                }
+            }
+
+            let mut new_bindings_reversed = Vec::new();
+
+            for (index, (type_, binding)) in exprs.into_iter().enumerate().rev() {
+                let annotated_binding =
+                    annotate_expr(binding, &current_future_usages, ids_in_scope + index);
+
+                let binding_move_info = annotated_binding.move_info.clone();
+
+                new_bindings_reversed.push((
+                    type_,
+                    annotated_binding,
+                    current_future_usages.clone(),
+                ));
+
+                for (var, _moves) in &binding_move_info.move_info {
+                    if var.0 < ids_in_scope {
+                        let entry = let_move_info.move_info.entry(*var).or_insert(0);
+                        if !current_future_usages.future_usages.contains(var) {
+                            *entry = 1;
+                        }
+                    }
+                }
+                for var in binding_move_info.move_info.keys() {
+                    current_future_usages.future_usages.insert(*var);
+                }
+
+                current_future_usages
+                    .future_usages
+                    .remove(&flat::LocalId(index + ids_in_scope));
+            }
+
+            let mut new_bindings = new_bindings_reversed;
+            new_bindings.reverse();
+
+            AnnotExpr {
+                kind: AnnotExprKind::LetMany(new_bindings, final_local),
+                move_info: let_move_info,
+            }
+        }
+
+        special::Expr::Branch(discrim, cases, res_type) => {
+            let mut branch_move_info = MoveInfo {
+                move_info: OrdMap::unit(discrim, 0),
+            };
+            if !future_usages.future_usages.contains(&discrim) {
+                branch_move_info.move_info.insert(discrim, 1);
+            }
+
+            let mut new_cases = Vec::new();
+
+            for (cond, body) in cases {
+                let annotated_body = annotate_expr(body, future_usages, ids_in_scope);
+
+                for (var, _move_count) in &annotated_body.move_info.move_info {
+                    let entry = branch_move_info.move_info.entry(*var).or_insert(0);
+                    if !future_usages.future_usages.contains(var) {
+                        *entry = 1;
+                    }
+                }
+
+                new_cases.push((cond, annotated_body));
+            }
+
+            AnnotExpr {
+                move_info: branch_move_info,
+                kind: AnnotExprKind::Branch(discrim, new_cases, res_type),
+            }
+        }
+
+        _ => AnnotExpr {
+            move_info: count_moves(&expr),
+            kind: AnnotExprKind::Leaf(expr),
+        },
+    }
+}
+
+impl MoveInfo {
+    fn add_borrow(&mut self, local_id: flat::LocalId) {
+        self.move_info.entry(local_id).or_insert(0);
+    }
+
+    fn add_move(&mut self, local_id: flat::LocalId) {
+        *self.move_info.entry(local_id).or_insert(0) += 1;
+    }
+}
+
+fn count_moves(expr: &special::Expr) -> MoveInfo {
+    todo![];
+    // let mut move_info = MoveInfo {
+    //     move_info: OrdMap::new(),
+    // };
+
+    // match expr {
+    // special::Expr::Local(local_id) => OrdSet::new(),
+    // special::Expr::Call(purity, func_id, local_id) => OrdSet::new(),
+
+    // special::Expr::Tuple(Vec<flat::LocalId>),
+    // special::Expr::TupleField(flat::LocalId, usize),
+    // special::Expr::WrapVariant(
+    // IdVec<first_ord::VariantId, Type>,
+    //  first_ord::VariantId,
+    //  flat::LocalId,
+    // ),
+    // special::Expr::UnwrapVariant(first_ord::VariantId, flat::LocalId),
+    // special::Expr::WrapCustom(CustomTypeId, flat::LocalId),
+    // special::Expr::UnwrapCustom(CustomTypeId, flat::LocalId),
+
+    // special::Expr::ArithOp(flat::ArithOp),
+    // special::Expr::ArrayOp(
+    //     constrain::RepChoice,
+    //     Type, // Item type
+    //     unif::ArrayOp,
+    // ),
+    // special::Expr::IoOp(constrain::RepChoice, flat::IOOp),
+
+    // special::Expr::ArrayLit(constrain::RepChoice, Type, Vec<flat::LocalId>),
+    // special::Expr::BoolLit(bool),
+    // special::Expr::ByteLit(u8),
+    // special::Expr::IntLit(i64),
+    // special::Expr::FloatLit(f64),
+    // }
+}
+
+// we need to add retain and releases
+// every time we have a syntactic occurence of a variable, we need to add a retain
+// we need to change branches to ifs
+// we need to add boxing/unboxing in places
 
 fn lower_structures(_program: special::Program) -> low::Program {
     todo![];
