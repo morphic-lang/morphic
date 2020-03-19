24ea4bc95447971c6985f780ecd2de066579afb3
Frank Dai <git@fyd.ai>
2020-03-18 22:28:29 -0700

    Add -a short flag for emit-artifacts

--- src/cli.rs
+++ src/cli.rs
@@ -97,11 +97,12 @@ impl Config {
                     .arg(
                         Arg::with_name("emit-artifacts")
                             .long("emit-artifacts")
+                            .short("a")
                             .help(
-                            "Emit compilation artifacts including the generated LLVM IR and \
-                             object file. Artifacts will be placed in a directory whose name is \
-                             derived from the generated executable's name.",
-                        ),
+                                "Emit compilation artifacts including the generated LLVM IR and \
+                                object file. Artifacts will be placed in a directory whose name is \
+                                derived from the generated executable's name.",
+                            ),
                     )
                     .arg(
                         Arg::with_name("target")
