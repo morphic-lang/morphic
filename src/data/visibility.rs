e450cc82311cfc8421b8e610684b002b089eb530
willtunnels <bendriscoll00@gmail.com>
2020-08-29 05:34:40 -0700

    Start impl of pub keyword

---
+++ src/data/visibility.rs
@@ -0,0 +1,4 @@
+pub enum Visibility {
+    Public,
+    Private,
+}
--- src/lex.rs
+++ src/lex.rs
@@ -29,6 +29,8 @@ pub enum Token {
     From,
     Expose,
     With,
+    As,
+    Pub,
 
     LParen,
     RParen,
@@ -99,6 +101,8 @@ impl fmt::Display for Token {
             Token::From => write!(f, "from"),
             Token::Expose => write!(f, "expose"),
             Token::With => write!(f, "with"),
+            Token::As => write!(f, "as"),
+            Token::Pub => write!(f, "pub"),
             Token::LParen => write!(f, "("),
             Token::RParen => write!(f, ")"),
             Token::LSquare => write!(f, "["),
@@ -359,6 +363,8 @@ impl<'a> Iterator for Lexer<'a> {
                 "from" => Some(Ok((name_start, Token::From, name_end))),
                 "expose" => Some(Ok((name_start, Token::Expose, name_end))),
                 "with" => Some(Ok((name_start, Token::With, name_end))),
+                "as" => Some(Ok((name_start, Token::As, name_end))),
+                "pub" => Some(Ok((name_start, Token::Pub, name_end))),
                 name => {
                     if char_at(name, 0).unwrap().is_uppercase() {
                         Some(Ok((
