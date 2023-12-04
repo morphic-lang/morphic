bdea5f5d4221cbfcfdd4343668972c5351cf3762
willtunnels <bendriscoll00@gmail.com>
2023-12-04 04:01:23 -0800

    Better find_clang error reporting

--- build.rs
+++ build.rs
@@ -4,35 +4,34 @@ use std::process::Command;
 use std::{env, fs};
 use walkdir::WalkDir;
 
-fn compile_tal() {
-    let cwd = env::current_dir().unwrap();
-    let clang = find_default_clang().unwrap();
+fn compile_tal() -> anyhow::Result<()> {
+    let cwd = env::current_dir()?;
+    let clang = find_default_clang().map_err(|err| anyhow::anyhow!("{err}"))?;
 
-    let wasm_out_dir = env::var("OUT_DIR").unwrap() + "/wasm";
-    let native_out_dir = env::var("OUT_DIR").unwrap() + "/native";
+    let wasm_out_dir = env::var("OUT_DIR")? + "/wasm";
+    let native_out_dir = env::var("OUT_DIR")? + "/native";
 
     if !Path::new(&wasm_out_dir).exists() {
-        fs::create_dir(&wasm_out_dir).unwrap();
+        fs::create_dir(&wasm_out_dir)?;
     }
     if !Path::new(&native_out_dir).exists() {
-        fs::create_dir(&native_out_dir).unwrap();
+        fs::create_dir(&native_out_dir)?;
     }
 
-    let native_tal_c = fs::canonicalize("./tal/native/tal.c").unwrap();
-    let wasm_tal_c = fs::canonicalize("./tal/wasm/src/tal.c").unwrap();
-    let wasm_malloc_c = fs::canonicalize("./tal/wasm/src/malloc.c").unwrap();
+    let native_tal_c = fs::canonicalize("./tal/native/tal.c")?;
+    let wasm_tal_c = fs::canonicalize("./tal/wasm/src/tal.c")?;
+    let wasm_malloc_c = fs::canonicalize("./tal/wasm/src/malloc.c")?;
 
     // Change the CWD so that clang outputs artifacts to the right location.
-    env::set_current_dir(native_out_dir).unwrap();
+    env::set_current_dir(native_out_dir)?;
     let status = Command::new(&clang.path)
         .arg("-c")
         .arg(native_tal_c)
-        .status()
-        .unwrap();
+        .status()?;
     assert!(status.success());
 
     // Change the CWD so that clang outputs artifacts to the right location.
-    env::set_current_dir(wasm_out_dir).unwrap();
+    env::set_current_dir(wasm_out_dir)?;
     let status = Command::new(&clang.path)
         .arg("-O3")
         .arg("-ffunction-sections")
@@ -47,25 +46,28 @@ fn compile_tal() {
         .arg("-c")
         .arg(wasm_tal_c)
         .arg(wasm_malloc_c)
-        .status()
-        .unwrap();
+        .status()?;
     assert!(status.success());
 
     // Make sure to reset the CWD, since lalrpop depends on it being the root.
-    env::set_current_dir(cwd).unwrap();
+    env::set_current_dir(cwd)?;
+
+    Ok(())
 }
 
-fn main() {
-    compile_tal();
+fn main() -> anyhow::Result<()> {
+    compile_tal()?;
 
     lalrpop::Configuration::new()
         .emit_rerun_directives(true)
         .process_current_dir()
-        .unwrap();
+        .map_err(|err| anyhow::anyhow!("{err}"))?;
 
     println!("cargo:rerun-if-changed=build.rs");
     for entry in WalkDir::new("tal") {
-        let entry = entry.unwrap();
+        let entry = entry?;
         println!("cargo:rerun-if-changed={}", entry.path().display());
     }
+
+    Ok(())
 }
