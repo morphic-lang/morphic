a6fedc90291e7e8a2a568546a0a43965ebfdd24d
willtunnels <bendriscoll00@gmail.com>
2024-03-28 10:26:23 -0700

    Scrap DynLazy types because of variance issues

--- src/data/mode_annot_ast2.rs
+++ src/data/mode_annot_ast2.rs
@@ -5,7 +5,6 @@ use crate::data::intrinsics::Intrinsic;
 use crate::data::profile as prof;
 use crate::data::purity::Purity;
 use crate::data::resolved_ast as res;
-use crate::util::lazy::{lazy, DynLazy};
 use id_collections::{id_type, Count, IdVec};
 use std::collections::BTreeSet;
 use std::hash::Hash;
@@ -216,44 +215,44 @@ impl LocalLt {
 #[derive(Clone, Debug)]
 pub struct Occur<M, L> {
     pub local: flat::LocalId,
-    pub ty: OldType<M, L>,
+    pub ty: Type<M, L>,
 }
 
 #[derive(Clone, Debug)]
 pub enum ArrayOp<M, L> {
     Get(
-        OldType<M, L>, // Item type (of input)
-        Occur<M, L>,   // Array
-        Occur<M, L>,   // Index
-        OldType<M, L>, // Return type; needed for RC op insertion
+        Type<M, L>,  // Item type (of input)
+        Occur<M, L>, // Array
+        Occur<M, L>, // Index
+        Type<M, L>,  // Return type; needed for RC op insertion
     ), // Returns item
     Extract(
-        OldType<M, L>, // Item type (of input)
-        Occur<M, L>,   // Array
-        Occur<M, L>,   // Index
+        Type<M, L>,  // Item type (of input)
+        Occur<M, L>, // Array
+        Occur<M, L>, // Index
     ), // Returns tuple of (item, hole array)
     Len(
-        OldType<M, L>, // Item type (of input)
-        Occur<M, L>,   // Array
+        Type<M, L>,  // Item type (of input)
+        Occur<M, L>, // Array
     ), // Returns int
     Push(
-        OldType<M, L>, // Item type (of input)
-        Occur<M, L>,   // Array
-        Occur<M, L>,   // Item
+        Type<M, L>,  // Item type (of input)
+        Occur<M, L>, // Array
+        Occur<M, L>, // Item
     ), // Returns new array
     Pop(
-        OldType<M, L>, // Item type (of input)
-        Occur<M, L>,   // Array
+        Type<M, L>,  // Item type (of input)
+        Occur<M, L>, // Array
     ), // Returns tuple (array, item)
     Replace(
-        OldType<M, L>, // Item type (of input)
-        Occur<M, L>,   // Hole array
-        Occur<M, L>,   // Item
+        Type<M, L>,  // Item type (of input)
+        Occur<M, L>, // Hole array
+        Occur<M, L>, // Item
     ), // Returns new array
     Reserve(
-        OldType<M, L>, // Item type (of input)
-        Occur<M, L>,   // Array
-        Occur<M, L>,   // Capacity
+        Type<M, L>,  // Item type (of input)
+        Occur<M, L>, // Array
+        Occur<M, L>, // Capacity
     ), // Returns new array
 }
 
@@ -267,31 +266,27 @@ pub enum IoOp<M, L> {
 pub enum Expr<M, L> {
     Local(Occur<M, L>),
     Call(Purity, first_ord::CustomFuncId, Occur<M, L>),
-    Branch(
-        Occur<M, L>,
-        Vec<(Condition<M, L>, Expr<M, L>)>,
-        OldType<M, L>,
-    ),
+    Branch(Occur<M, L>, Vec<(Condition<M, L>, Expr<M, L>)>, Type<M, L>),
     LetMany(
-        Vec<(OldType<M, L>, Expr<M, L>)>, // bound values.  Each is assigned a new sequential Occur<M,L>
-        Occur<M, L>,                      // body
+        Vec<(Type<M, L>, Expr<M, L>)>, // bound values.  Each is assigned a new sequential Occur<M,L>
+        Occur<M, L>,                   // body
     ),
 
     Tuple(Vec<Occur<M, L>>),
     TupleField(Occur<M, L>, usize),
     WrapVariant(
-        IdVec<first_ord::VariantId, OldType<M, L>>,
+        IdVec<first_ord::VariantId, Type<M, L>>,
         first_ord::VariantId,
         Occur<M, L>,
     ),
     UnwrapVariant(first_ord::VariantId, Occur<M, L>),
     WrapBoxed(
         Occur<M, L>,
-        OldType<M, L>, // Inner type
+        Type<M, L>, // Inner type
     ),
     UnwrapBoxed(
         Occur<M, L>,
-        OldType<M, L>, // Inner type
+        Type<M, L>, // Inner type
     ),
     WrapCustom(first_ord::CustomTypeId, Occur<M, L>),
     UnwrapCustom(first_ord::CustomTypeId, Occur<M, L>),
@@ -299,9 +294,9 @@ pub enum Expr<M, L> {
     Intrinsic(Intrinsic, Occur<M, L>),
     ArrayOp(ArrayOp<M, L>),
     IoOp(IoOp<M, L>),
-    Panic(OldType<M, L>, Occur<M, L>),
+    Panic(Type<M, L>, Occur<M, L>),
 
-    ArrayLit(OldType<M, L>, Vec<Occur<M, L>>),
+    ArrayLit(Type<M, L>, Vec<Occur<M, L>>),
     BoolLit(bool),
     ByteLit(u8),
     IntLit(i64),
@@ -315,7 +310,7 @@ pub enum Condition<M, L> {
     Variant(first_ord::VariantId, Box<Condition<M, L>>),
     Boxed(
         Box<Condition<M, L>>,
-        OldType<M, L>, // Inner type
+        Type<M, L>, // Inner type
     ),
     Custom(first_ord::CustomTypeId, Box<Condition<M, L>>),
     BoolConst(bool),
@@ -325,11 +320,11 @@ pub enum Condition<M, L> {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum OldOverlay<M> {
+pub enum Overlay<M> {
     Bool,
     Num(first_ord::NumType),
-    Tuple(Vec<OldOverlay<M>>),
-    Variants(IdVec<first_ord::VariantId, OldOverlay<M>>),
+    Tuple(Vec<Overlay<M>>),
+    Variants(IdVec<first_ord::VariantId, Overlay<M>>),
     // The mode and lifetime substitutions for this custom type's parameters are stored in the type
     // which corresponds to this overlay.
     Custom(first_ord::CustomTypeId),
@@ -339,61 +334,57 @@ pub enum OldOverlay<M> {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum OldType<M, L> {
+pub enum Type<M, L> {
     Bool,
     Num(first_ord::NumType),
-    Tuple(Vec<OldType<M, L>>),
-    Variants(IdVec<first_ord::VariantId, OldType<M, L>>),
+    Tuple(Vec<Type<M, L>>),
+    Variants(IdVec<first_ord::VariantId, Type<M, L>>),
     Custom(
         first_ord::CustomTypeId,
         IdVec<ModeParam, M>,
         IdVec<LtParam, L>,
     ),
-    Array(M, L, Box<OldType<M, L>>, OldOverlay<M>),
-    HoleArray(M, L, Box<OldType<M, L>>, OldOverlay<M>),
-    Boxed(M, L, Box<OldType<M, L>>, OldOverlay<M>),
+    Array(M, L, Box<Type<M, L>>, Overlay<M>),
+    HoleArray(M, L, Box<Type<M, L>>, Overlay<M>),
+    Boxed(M, L, Box<Type<M, L>>, Overlay<M>),
 }
 
-impl<M: Clone> OldType<M, Lt> {
+impl<M: Clone> Type<M, Lt> {
     // Returns the meet of two types over the lifetime lattice. This meet is "left" in the sense
     // that the modes of the output are taken from the first argument.
     pub fn left_meet(&self, other: &Self) -> Self {
         match (self, other) {
-            (OldType::Bool, OldType::Bool) => OldType::Bool,
-            (OldType::Num(n1), OldType::Num(n2)) if n1 == n2 => OldType::Num(*n1),
-            (OldType::Tuple(tys1), OldType::Tuple(tys2)) => OldType::Tuple(
+            (Type::Bool, Type::Bool) => Type::Bool,
+            (Type::Num(n1), Type::Num(n2)) if n1 == n2 => Type::Num(*n1),
+            (Type::Tuple(tys1), Type::Tuple(tys2)) => Type::Tuple(
                 tys1.iter()
                     .zip(tys2)
                     .map(|(t1, t2)| t1.left_meet(t2))
                     .collect(),
             ),
-            (OldType::Custom(id1, ms1, lts1), OldType::Custom(id2, _ms2, _lts2)) if id1 == id2 => {
-                OldType::Custom(*id1, ms1.clone(), lts1.clone())
+            (Type::Custom(id1, ms1, lts1), Type::Custom(id2, _ms2, _lts2)) if id1 == id2 => {
+                Type::Custom(*id1, ms1.clone(), lts1.clone())
             }
-            (OldType::Array(m1, lt1, ty1, o1), OldType::Array(_m2, lt2, ty2, _o2)) => {
-                OldType::Array(
+            (Type::Array(m1, lt1, ty1, o1), Type::Array(_m2, lt2, ty2, _o2)) => Type::Array(
+                m1.clone(),
+                lt1.join(lt2),
+                Box::new(ty1.left_meet(ty2)),
+                o1.clone(),
+            ),
+            (Type::HoleArray(m1, lt1, ty1, o1), Type::HoleArray(_m2, lt2, ty2, _o2)) => {
+                Type::HoleArray(
                     m1.clone(),
                     lt1.join(lt2),
                     Box::new(ty1.left_meet(ty2)),
                     o1.clone(),
                 )
             }
-            (OldType::HoleArray(m1, lt1, ty1, o1), OldType::HoleArray(_m2, lt2, ty2, _o2)) => {
-                OldType::HoleArray(
-                    m1.clone(),
-                    lt1.join(lt2),
-                    Box::new(ty1.left_meet(ty2)),
-                    o1.clone(),
-                )
-            }
-            (OldType::Boxed(m1, lt1, ty1, o1), OldType::Boxed(_m2, lt2, ty2, _o2)) => {
-                OldType::Boxed(
-                    m1.clone(),
-                    lt1.join(lt2),
-                    Box::new(ty1.left_meet(ty2)),
-                    o1.clone(),
-                )
-            }
+            (Type::Boxed(m1, lt1, ty1, o1), Type::Boxed(_m2, lt2, ty2, _o2)) => Type::Boxed(
+                m1.clone(),
+                lt1.join(lt2),
+                Box::new(ty1.left_meet(ty2)),
+                o1.clone(),
+            ),
             _ => panic!("incompatible types"),
         }
     }
@@ -406,8 +397,8 @@ pub struct Sig {
     pub num_overlay_mode_params: Count<ModeParam>,
     pub num_lt_params: Count<LtParam>,
 
-    pub arg_type: OldType<ModeParam, LtParam>,
-    pub ret_type: OldType<ModeParam, LtParam>,
+    pub arg_type: Type<ModeParam, LtParam>,
+    pub ret_type: Type<ModeParam, LtParam>,
     pub constrs: Vec<ModeConstr<ModeParam>>,
 }
 
@@ -427,8 +418,8 @@ pub struct CustomTypeDef {
     pub num_mode_params: Count<ModeParam>,
     pub num_overlay_mode_params: Count<ModeParam>,
     pub num_lt_params: Count<LtParam>,
-    pub content: OldType<ModeParam, LtParam>,
-    pub overlay: OldOverlay<ModeParam>,
+    pub content: Type<ModeParam, LtParam>,
+    pub overlay: Overlay<ModeParam>,
 }
 
 #[derive(Clone, Debug)]
@@ -447,174 +438,3 @@ pub struct Program {
     pub profile_points: IdVec<prof::ProfilePointId, prof::ProfilePoint>,
     pub main: first_ord::CustomFuncId,
 }
-
-// The F-algebra for types
-pub enum TF<A, B, X, Y> {
-    Bool,
-    Num(first_ord::NumType),
-    Tuple(Vec<Y>),
-    Variants(IdVec<first_ord::VariantId, Y>),
-    Custom(first_ord::CustomTypeId, A),
-    Array(B, X, Y),
-    HoleArray(B, X, Y),
-    Boxed(B, X, Y),
-}
-
-// The F-algebra for "simple" types; used when all we care about is the shape of the type
-pub enum STF<X> {
-    Bool,
-    Num(first_ord::NumType),
-    Tuple(Vec<X>),
-    Variants(IdVec<first_ord::VariantId, X>),
-    Custom(first_ord::CustomTypeId),
-    Array,
-    HoleArray,
-    Boxed,
-}
-
-// The F-algebra for overlays
-#[derive(Clone, Debug)]
-pub enum OF<A, B, X> {
-    Bool,
-    Num(first_ord::NumType),
-    Tuple(Vec<X>),
-    Variants(IdVec<first_ord::VariantId, X>),
-    Custom(first_ord::CustomTypeId, A),
-    Array(B),
-    HoleArray(B),
-    Boxed(B),
-}
-
-pub trait VecCompat {
-    type Item;
-
-    fn map_refs<U, F>(&self, f: F) -> Vec<U>
-    where
-        F: FnMut(usize, &Self::Item) -> U;
-}
-
-impl<T> VecCompat for Vec<T> {
-    type Item = T;
-
-    fn map_refs<U, F>(&self, mut f: F) -> Vec<U>
-    where
-        F: FnMut(usize, &Self::Item) -> U,
-    {
-        self.iter().enumerate().map(|(i, v)| f(i, v)).collect()
-    }
-}
-
-pub struct TL<'a, A, B, C, D>(TF<A, B, OL<'a, C, D>, DynLazy<'a, TL<'a, A, B, C, D>>>);
-pub struct STL<'a>(STF<DynLazy<'a, STL<'a>>>);
-pub struct OL<'a, A, B>(OF<A, B, DynLazy<'a, OL<'a, A, B>>>);
-
-pub type Type<M, L> =
-    TL<'static, (IdVec<ModeParam, M>, IdVec<LtParam, L>), (M, L), IdVec<ModeParam, M>, M>;
-pub type Overlay<M> = OL<'static, IdVec<ModeParam, M>, M>;
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum DataKind {
-    Stack,
-    Heap,
-}
-
-// impl<'a, A: 'static, B: 'static> OL<'a, A, B> {
-//     pub fn map<A2, B2, F1, F2>(&self, f1: F1, f2: F2) -> OL<'a, A2, B2>
-//     where
-//         F1: Fn(&A) -> A2 + Clone + 'a,
-//         F2: Fn(&B) -> B2 + Clone + 'a,
-//     {
-//         // `DataKind::Stack` is just a dummy value here. `DataKind` is only relevant when the
-//         // overlay resides inside a type, in which case `map_rec` will be called directly.
-//         self.map_rec(DataKind::Stack, move |_, v| f1(v), move |_, v| f2(v))
-//     }
-//
-//     fn map_rec<A2, B2, F1, F2>(&self, kind: DataKind, f1: F1, f2: F2) -> OL<'a, A2, B2>
-//     where
-//         F1: Fn(DataKind, &A) -> A2 + Clone + 'a,
-//         F2: Fn(DataKind, &B) -> B2 + Clone + 'a,
-//     {
-//         let recurse = |ty: &DynLazy<OL<A, B>>, kind: DataKind| {
-//             let ty: DynLazy<_> = ty.clone();
-//             let f1 = f1.clone();
-//             let f2 = f2.clone();
-//             lazy(move || ty.map_rec(kind, f1, f2))
-//         };
-//
-//         match &self.0 {
-//             OF::Bool => OL(OF::Bool),
-//             OF::Num(n) => OL(OF::Num(*n)),
-//             OF::Tuple(tys) => OL(OF::Tuple(tys.map_refs(|_, ty| recurse(ty, kind)))),
-//             OF::Variants(tys) => OL(OF::Variants(tys.map_refs(|_, ty| recurse(ty, kind)))),
-//             OF::Custom(id, tys) => OL(OF::Custom(*id, f1(DataKind::Stack, tys))),
-//             OF::Array(v) => OL(OF::Array(f2(kind, v))),
-//             OF::HoleArray(v) => OL(OF::HoleArray(f2(kind, v))),
-//             OF::Boxed(v) => OL(OF::Boxed(f2(kind, v))),
-//         }
-//     }
-// }
-
-// impl<'a, A: 'a, B: 'a, C: 'a, D: 'a> TL<'a, A, B, C, D> {
-//     pub fn map<A2, B2, C2, D2, F1, F2, F3, F4>(
-//         &self,
-//         f1: F1,
-//         f2: F2,
-//         f3: F3,
-//         f4: F4,
-//     ) -> TL<'a, A2, B2, C2, D2>
-//     where
-//         F1: Fn(DataKind, &A) -> A2 + Clone + 'a,
-//         F2: Fn(DataKind, &B) -> B2 + Clone + 'a,
-//         F3: Fn(DataKind, &C) -> C2 + Clone + 'a,
-//         F4: Fn(DataKind, &D) -> D2 + Clone + 'a,
-//     {
-//         self.map_rec(DataKind::Stack, f1, f2, f3, f4)
-//     }
-//
-//     fn map_rec<A2, B2, C2, D2, F1, F2, F3, F4>(
-//         &self,
-//         kind: DataKind,
-//         f1: F1,
-//         f2: F2,
-//         f3: F3,
-//         f4: F4,
-//     ) -> TL<'a, A2, B2, C2, D2>
-//     where
-//         F1: Fn(DataKind, &A) -> A2 + Clone + 'static,
-//         F2: Fn(DataKind, &B) -> B2 + Clone + 'static,
-//         F3: Fn(DataKind, &C) -> C2 + Clone + 'static,
-//         F4: Fn(DataKind, &D) -> D2 + Clone + 'static,
-//     {
-//         let recurse = |ty: &DynLazy<TL<A, B, C, D>>, kind: DataKind| {
-//             let ty = ty.clone();
-//             let f1 = f1.clone();
-//             let f2 = f2.clone();
-//             let f3 = f3.clone();
-//             let f4 = f4.clone();
-//             lazy(move || ty.map_rec(kind, f1, f2, f3, f4))
-//         };
-//
-//         match &self.0 {
-//             TF::Bool => TL(TF::Bool),
-//             TF::Num(n) => TL(TF::Num(*n)),
-//             TF::Tuple(tys) => TL(TF::Tuple(tys.map_refs(|_, ty| recurse(ty, kind)))),
-//             TF::Variants(tys) => TL(TF::Variants(tys.map_refs(|_, ty| recurse(ty, kind)))),
-//             TF::Custom(id, tys) => TL(TF::Custom(*id, f1(kind, tys))),
-//             TF::Array(v, ov, ty) => TL(TF::Array(
-//                 f2(kind, v),
-//                 ov.map_rec(kind, f3.clone(), f4.clone()),
-//                 recurse(ty, DataKind::Heap),
-//             )),
-//             TF::HoleArray(v, ov, ty) => TL(TF::HoleArray(
-//                 f2(kind, v),
-//                 ov.map_rec(kind, f3.clone(), f4.clone()),
-//                 recurse(ty, DataKind::Heap),
-//             )),
-//             TF::Boxed(v, ov, ty) => TL(TF::Boxed(
-//                 f2(kind, v),
-//                 ov.map_rec(kind, f3.clone(), f4.clone()),
-//                 recurse(ty, DataKind::Heap),
-//             )),
-//         }
-//     }
-// }
